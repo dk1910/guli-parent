2ec3b04945a0a06e194a9dbf531ddb667157105f
Daike <1146741043@qq.com>
2021-12-13 02:13:07 +0800

    代码生成器

---
+++ service/service_edu/src/main/java/com/atguigu/eduservice/EduApplication.java
@@ -0,0 +1,16 @@
+package com.atguigu.eduservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author daike
+ * @date {2021/12/10} {3:24}
+ */
+@SpringBootApplication
+public class EduApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EduApplication.class,args);
+    }
+}
